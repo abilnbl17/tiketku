ae39b3fe6b163e9e18a3e13e8cc6fa29f72c0eb8
abilnbl17 <abilabil17@gmail.com>
2024-04-16 22:40:38 +0700

    feat: add color background

--- apps/web/src/components/EventList/Hero.tsx
+++ apps/web/src/components/EventList/Hero.tsx
@@ -1,12 +1,15 @@
+'use client';
+
 import React from 'react';
 
 const Hero = () => {
   return (
-    <div className="flex items-center bg-[#14233c] bg-opacity-60">
-      <section
-        style={{ backgroundImage: 'url(/hero.jpg)' }}
-        className="w-full bg-cover bg-center py-32 "
-      >
+    // <div className="flex items-center bg-black">
+    <section
+      className="w-full bg-cover bg-center "
+      style={{ backgroundImage: 'url(/hero.jpg)' }}
+    >
+      <div className="flex items-center h-full px-20 py-40 bg-gray-900 bg-opacity-40">
         <div className="container relative mx-auto text-center text-black">
           {/* <div className="container"> */}
           <div className="justify-center row-auto">
@@ -14,7 +17,7 @@ const Hero = () => {
             <h1 className="text-6xl font-bold mb-6 text-white">
               Discover Events & Workshops Around You
             </h1>
-            <p class="text-xl mb-12 text-white">
+            <p className="text-xl mb-12 text-white">
               Dont miss out on the experience of a lifetime - secure your spot
               at your preferable events with our easy online ticket purchasing
               system and join it for building a good community.
@@ -22,17 +25,18 @@ const Hero = () => {
             <button>
               <a
                 href="#"
-                class="bg-indigo-500 text-white py-1 px-12 rounded-full hover:bg-indigo-600"
+                className="bg-indigo-500 text-white py-1 px-12 rounded-full hover:bg-indigo-600"
               >
                 Demo
               </a>
             </button>
           </div>
         </div>
-        {/* </div> */}
-        {/* </div> */}
-      </section>
-    </div>
+      </div>
+    </section>
+    // {/* </div> */}
+    // {/* </div> */}
+    // </div>
   );
 };
 
